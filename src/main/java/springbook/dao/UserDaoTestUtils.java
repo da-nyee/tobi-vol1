e3bb399f37a5553611b58a6048dd6b61fc3c3e55
Daeun Lee <leede0418@likelion.org>
2021-05-24 02:48:49 +0900

    test: 테스트 검증 자동화

--- src/main/java/springbook/dao/UserDaoTestUtils.java
+++ src/main/java/springbook/dao/UserDaoTestUtils.java
@@ -17,9 +17,15 @@ public class UserDaoTestUtils {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+            return;
+        }
+        if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+            return;
+        }
+        System.out.println("조회 테스트 성공");
     }
 }
